e8fb171d94958b390a41a9ca7e8b2c3bdd46a536
Igor Chaves Donega <ichaves@live.com>
2024-01-11 15:00:18 +0900

    ✅ Added react route dependencies

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
-const App = () => {
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 
-  return (
-    <></>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route to="/about" element={<About />} />
+      <Route to="/contact" element={<Contact />} />
+      <Route to="/cart" element={<Cart />} />
+    </Route>
   )
-}
+)
+
+const App = () => <RouterProvider router={router} />
 
 export { App }
