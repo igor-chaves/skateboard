35f5d447238caa6fafa88da4d781428a622fb884
Igor Chaves Donega <ichaves@live.com>
2024-01-31 22:53:53 +0900

    ✅ layout of button in contact page for iPhone is the same as in Desktop now

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -15,10 +15,11 @@ const Contact = () => {
       email: e.target[1].value,
       message: e.target[2].value
     }
+
     const isValid = await formSchema.isValid(formData)
 
     if (isValid) {
-      alert("We received your message, we'll be in touch soon")
+      alert("We received your message and will be in touch soon.")
       formRef.current.reset()
     } else {
       alert("Please fill all the fields correctly")
@@ -26,35 +27,33 @@ const Contact = () => {
   }
 
   return (
-    <>
-      <main>
-        <section>
-          <div className="container flex">
-            <form className="form-contact" ref={formRef} onSubmit={handleForm}>
-              <div className="form-top">
-                <h1>CONTACT</h1>
-              </div>
+    <main>
+      <section>
+        <div className="container flex">
+          <form className="form-contact" ref={formRef} onSubmit={handleForm}>
+            <div className="form-top">
+              <h1>CONTACT</h1>
+            </div>
 
-              <div id="contact-form-inputs" className="form-mid">
-                <input name="name" type="text" placeholder="Full name" autoComplete="off" />
-                <input name="email" type="text" placeholder="E-mail" autoComplete="off" />
-                <textarea
-                  name="message"
-                  placeholder="Your message"
-                  rows="4" cols="4"
-                  maxLength="200"
-                  autoComplete="off">
-                </textarea>
-              </div>
+            <div id="contact-form-inputs" className="form-mid">
+              <input name="name" type="text" placeholder="Full name" autoComplete="off" />
+              <input name="email" type="text" placeholder="E-mail" autoComplete="off" />
+              <textarea
+                name="message"
+                placeholder="Your message"
+                rows="4" cols="4"
+                maxLength="200"
+                autoComplete="off">
+              </textarea>
+            </div>
 
-              <div className="send-btn flex">
-                <button className="send-contact" type="submit">Send</button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
-    </>
+            <div className="send-btn flex">
+              <button className="send-contact" type="submit">Send</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
   )
 }
 
