fe8349ed8ae2d07f6ed9769babeed1dd47301d64
Igor Chaves Donega <ichaves@live.com>
2024-02-25 17:35:42 +0900

    ✅ changed CSS from buttons on cart component

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import localforage from "localforage"
 import "./cart.css"
+import { Link } from "react-router-dom"
 
 const Cart = () => {
   const [cart, setCart] = useState([])
@@ -17,37 +18,45 @@ const Cart = () => {
     <div className="main-container">
       {/* list of products column */}
       <div className="column-container">
-        {cart.map(({ product, id, price }) => (
-          <div className="product-card" key={id}>
-            <div className="product-content">
-              <div className="img-container">
-                <div className="product-image">
-                  <img src={`../deck${id}.jpg`} alt={`${product}`} className="product-img" />
+        {cart.length > 0
+          ? cart.map(({ product, id, price }) => (
+            <div className="product-card" key={id}>
+              <div className="product-content">
+                <div className="img-container">
+                  <div className="product-image">
+                    <img src={`../deck${id}.jpg`} alt={`${product}`} className="product-img" />
+                  </div>
                 </div>
-              </div>
 
-              <div className="product-infos">
-                <div className="product-details">
-                  <h3 className="product-title">{product}</h3>
-                  <p className="product-info">Color: Black</p>
-                  <p className="product-info">Size: M</p>
+                <div className="product-infos">
+                  <div className="product-details">
+                    <h3 className="product-title">{product}</h3>
+                    <p className="product-info">Color: Black</p>
+                    <p className="product-info">Size: M</p>
+                  </div>
+                  <div className="product-price">
+                    <p className="price">${price}</p>
+                    <p className="product-code">Item #{id}</p>
+                  </div>
                 </div>
-                <div className="product-price">
-                  <p className="price">${price}</p>
-                  <p className="product-code">Item #{id}</p>
-                </div>
-              </div>
 
-              <button className="remove-button">
-                <svg className="remove-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M3 6h18"></path>
-                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-                </svg>
-              </button>
+                <button className="remove-button">
+                  <svg className="remove-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M3 6h18"></path>
+                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                  </svg>
+                </button>
+              </div>
             </div>
+          ))
+          :
+          <div className="empty-cart">
+            <h3>Your cart is empty</h3><br />
+            <p><Link to="/"><button className="btn">Check products</button></Link></p>
           </div>
-        ))}
+
+        }
       </div>
 
       {/* billing column */}
